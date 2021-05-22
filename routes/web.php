1a92fd76db20bfd98c3657cab0a5e445e8e591c4
hhyzhanglei <605060306@qq.com>
2021-05-22 20:36:25 +0800

    nihao

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/cesh', function () {
+Route::get('/nihao', function () {
     return 'welcome to laravel';
 });
