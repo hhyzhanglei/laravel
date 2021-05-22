9ac2d2f5108d1b7c4f35d7562dcf33d9dc0274af
zhanglei <605060306@qq.com>
2021-05-22 20:03:10 +0800

    测试

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/zhanglei', function () {
+Route::get('/ceshi', function () {
     return 'welcome to laravel';
 });
