89e46408638fd8fef5d280bf5896fc5f3a640112
hhyzhanglei <605060306@qq.com>
2021-05-23 21:41:17 +0800

    任务调度

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -8,27 +8,13 @@ use App\Models\Order;
 use App\Models\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\Redis;
 class TestController extends Controller
 {
     public function webhook(Request $request){
         Log::notice('查看日志：'.json_encode($request));
     }
     public function add(){
-        $all_user = Users::query()->pluck('id')->toArray();
-        $user_id = array_rand($all_user,1);
-        Log::notice('用户id'.$user_id);
-        $all_goods = Goods::query()->pluck('id')->toArray();
-        $goods_id = array_rand($all_goods,1);
-        Log::notice('商品id'.$goods_id);
-        $order_number = date('YmdHis') . str_pad(mt_rand(1, 99999), 5, '0', STR_PAD_LEFT);
-        Log::notice('生成订单编号'.$order_number);
-        $create = [
-            'user_id' => $user_id,
-            'goods_id' => $goods_id,
-            'number' => $order_number,
-            'status' => 1
-        ];
-        Order::query()->create($create);
+        Redis::set('name','Tar');
     }
 }
