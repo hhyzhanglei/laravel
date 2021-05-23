0df78d4a5aca193799adf006fff0813f6b77beb4
hhyzhanglei <605060306@qq.com>
2021-05-23 20:49:51 +0800

    任务调度

--- app/Console/Commands/GenerateOrder.php
+++ app/Console/Commands/GenerateOrder.php
@@ -6,6 +6,7 @@ use App\Models\Goods;
 use App\Models\Order;
 use App\Models\Users;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class GenerateOrder extends Command
 {
@@ -41,9 +42,12 @@ class GenerateOrder extends Command
     {
         $all_user = Users::query()->pluck('id')->toArray();
         $user_id = array_rand($all_user,1);
+        Log::notice('用户id'.$user_id);
         $all_goods = Goods::query()->pluck('id')->toArray();
         $goods_id = array_rand($all_goods,1);
+        Log::notice('商品id'.$goods_id);
         $order_number = date('YmdHis') . str_pad(mt_rand(1, 99999), 5, '0', STR_PAD_LEFT);
+        Log::notice('生成订单编号'.$order_number);
         $create = [
             'user_id' => $user_id,
             'goods_id' => $goods_id,
