02c2a7784f7982fc1d6d79fb8ff696cd21f45327
hhyzhanglei <605060306@qq.com>
2021-05-23 19:25:52 +0800

    1111

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -1,5 +1,14 @@
 <?php
-$dir = '/usr/share/nginx/html/laravel';
-echo shell_exec("cd /usr/share/nginx/html/laravel");
-echo shell_exec("sudo git pull 2>&1");
-?>
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class TestController extends Controller
+{
+    public function webhook(Request $request){
+        Log::notice('查看日志：'.json_encode($request));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\Api\TestController;
 
-Route::any('/', function () {
+Route::any('/index', function () {
     return view('welcome');
 });
 Route::group([
