337cb04f6ba4e46ddba8ee4e63475fb48f5dfd56
hhyzhanglei <605060306@qq.com>
2021-05-23 18:56:23 +0800

    success

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -1,15 +1,5 @@
 <?php
-
-namespace App\Http\Controllers\Api;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-
-class TestController extends Controller
-{
-    public function webhook(Request $request){
-        Log::notice('查看日志：'.json_encode($request));
-        return 'success';
-    }
-}
+$dir = '/usr/share/nginx/html/laravel';
+echo shell_exec("cd /usr/share/nginx/html/laravel");
+echo shell_exec("sudo git pull 2>&1");
+?>
